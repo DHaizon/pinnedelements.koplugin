d6d6ddff436bc01b6b39c7ad1512641043d709a4
Pan Zhong Qin <fps80391@gmail.com>
2026-07-02 09:19:21 -0500

    Delete _meta.lua

--- _meta.lua
+++
@@ -1,6 +0,0 @@
-local _ = require("gettext")
-return {
-    name = "pinnedelements",
-    fullname = _("Pinned Elements"),
-    description = _([[Pin pages and text for quick popup reference anywhere in the book.]]),
-}
